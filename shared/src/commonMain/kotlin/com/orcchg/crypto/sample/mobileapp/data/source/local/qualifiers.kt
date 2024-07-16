3de5d0e84cb9001a0c58eab0b2398ea6c3045f8c
orcchg <prism.getprism@gmail.com>
2024-07-16 18:37:05 +0400

    Provide local and remote data sources to DI

---
+++ shared/src/commonMain/kotlin/com/orcchg/crypto/sample/mobileapp/data/source/local/qualifiers.kt
@@ -0,0 +1,6 @@
+package com.orcchg.crypto.sample.mobileapp.data.source.local
+
+enum class Qualifier {
+    IN_MEMORY,
+    PERSISTENT
+}
--- shared/src/commonMain/kotlin/com/orcchg/crypto/sample/mobileapp/di/modules.kt
+++ shared/src/commonMain/kotlin/com/orcchg/crypto/sample/mobileapp/di/modules.kt
@@ -1,20 +1,47 @@
 package com.orcchg.crypto.sample.mobileapp.di
 
 import com.orcchg.crypto.sample.mobileapp.data.repository.RealCryptoRepository
+import com.orcchg.crypto.sample.mobileapp.data.source.local.CoinsDatabaseFacade
+import com.orcchg.crypto.sample.mobileapp.data.source.local.Qualifier
+import com.orcchg.crypto.sample.mobileapp.data.source.local.backend.InMemoryCoinsDatabaseFacade
+import com.orcchg.crypto.sample.mobileapp.data.source.local.backend.RealCoinsDatabaseFacade
+import com.orcchg.crypto.sample.mobileapp.data.source.remote.CoinsRemoteFacade
 import com.orcchg.crypto.sample.mobileapp.data.source.remote.backend.BackendPreferences
+import com.orcchg.crypto.sample.mobileapp.data.source.remote.backend.RealCoinsRemoteFacade
 import com.orcchg.crypto.sample.mobileapp.domain.repository.CryptoRepository
+import org.koin.core.annotation.KoinInternalApi
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
-val serviceLocatorModule = module {
+internal val localDataSourceModule = module {
+    single<CoinsDatabaseFacade>(named(Qualifier.IN_MEMORY)) {
+        InMemoryCoinsDatabaseFacade()
+    }
+
+    single<CoinsDatabaseFacade>(named(Qualifier.PERSISTENT)) {
+        RealCoinsDatabaseFacade()
+    }
+}
+
+internal val remoteDataSourceModule = module {
     single { BackendPreferences() }
+    single<CoinsRemoteFacade> {
+        RealCoinsRemoteFacade(backendPreferences = get())
+    }
+}
+
+internal val serviceLocatorModule = module {
+    includes(
+        localDataSourceModule,
+        remoteDataSourceModule
+    )
     single<CryptoRepository> {
         RealCryptoRepository(
-            backendPreferences = get()
+            local = get(qualifier = named(Qualifier.IN_MEMORY)),
+            remote = get()
         )
     }
     single<ServiceLocator> {
-        RealServiceLocator(
-            cryptoRepository = get()
-        )
+        RealServiceLocator(cryptoRepository = get())
     }
 }
