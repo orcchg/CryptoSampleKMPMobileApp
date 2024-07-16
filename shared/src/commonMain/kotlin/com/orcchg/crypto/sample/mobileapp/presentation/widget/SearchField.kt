32d12f7a7bc8bbd0b9ca61b895979bb085dfb03e
orcchg <prism.getprism@gmail.com>
2024-07-16 21:02:18 +0400

    Finalize search field

---
+++ shared/src/commonMain/kotlin/com/orcchg/crypto/sample/mobileapp/presentation/widget/SearchField.kt
@@ -0,0 +1,110 @@
+package com.orcchg.crypto.sample.mobileapp.presentation.widget
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.orcchg.crypto.sample.mobileapp.presentation.theme.Colors
+import cryptosamplekmpmobileapp.shared.generated.resources.Res
+import cryptosamplekmpmobileapp.shared.generated.resources.ic_back
+import cryptosamplekmpmobileapp.shared.generated.resources.ic_close
+import cryptosamplekmpmobileapp.shared.generated.resources.ic_search
+import cryptosamplekmpmobileapp.shared.generated.resources.label_search_hint
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.resources.vectorResource
+
+@Composable
+fun SearchField(
+    input: String = "",
+    isFocused: Boolean = false,
+    modifier: Modifier = Modifier,
+    onBackPressed: () -> Unit = {},
+    onClearPressed: () -> Unit = {},
+    onSearchInput: (input: String) -> Unit = {}
+) {
+    val (borderWidth, icon) = if (isFocused) {
+        2.dp to Res.drawable.ic_back
+    } else {
+        1.dp to Res.drawable.ic_search
+    }
+    Box(
+        modifier = modifier
+            .requiredHeight(48.dp)
+            .background(
+                color = Colors.backgroundColor,
+                shape = RoundedCornerShape(40.dp)
+            )
+            .border(
+                width = borderWidth,
+                color = Colors.textColor,
+                shape = RoundedCornerShape(40.dp)
+            )
+            .padding(vertical = 12.dp, horizontal = 16.dp),
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(
+                onClick = onBackPressed,
+                modifier = Modifier.size(24.dp)
+            ) {
+                Icon(
+                    imageVector = vectorResource(icon),
+                    contentDescription = null,
+                    tint = Colors.textColor
+                )
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            OutlinedTextField(
+                value = input,
+                onValueChange = onSearchInput,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1.0f),
+                textStyle = MaterialTheme.typography.subtitle1,
+                placeholder = {
+                    Text(
+                        text = stringResource(Res.string.label_search_hint),
+                        color = Colors.textColor,
+                        fontWeight = FontWeight.SemiBold,
+                        style = MaterialTheme.typography.subtitle1
+                    )
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                singleLine = true
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            IconButton(
+                onClick = onClearPressed,
+                modifier = Modifier.size(24.dp)
+            ) {
+                Icon(
+                    imageVector = vectorResource(Res.drawable.ic_close),
+                    contentDescription = null,
+                    tint = Colors.textColor
+                )
+            }
+        }
+    }
+}
